625b31c35d67fcce42b1bb213ac3976a005afdf4
Valentina Rua <valentina.rua123@gmail.com>
2019-03-10 21:42:35 -0500

    nueva tarea

--- src/containers/tareas.js
+++ src/containers/tareas.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, FlatList, Button, Text} from 'react-native';
+import { View, FlatList, Button, Text, StyleSheet} from 'react-native';
 import uuid from 'uuid';
 
 
@@ -7,11 +7,11 @@ class ListaTareas extends Component {
 
     state = {
         tareas : [
-            {titulo: 'hooks', dia: 'lunes', hora: '5:00',fecha: '8 marzo 2019',id: uuid() },
-            {titulo: 'redux',dia: 'martes',hora:'6:00',fecha:'9 marzo 2019',id: uuid()},
-            {titulo: 'navigatio', dia: 'miercoles',hora:'7:00',fecha:'10 marzo 2019',id: uuid()},
-            {titulo: 'fundamentsJS', dia: 'jueves',hora:'8:00',fecha:'11 marzo 2019',id: uuid()},
-            {titulo:  'native', dia: 'viernes',hora:'9:00',fecha:'12 marzo 2019',id: uuid()}
+            {titulo: 'Hooks', dia: 'lunes', hora: '5:00',fecha: '8 marzo 2019',id: uuid() },
+            {titulo: 'Redux',dia: 'martes',hora:'6:00',fecha:'9 marzo 2019',id: uuid()},
+            {titulo: 'Navigation', dia: 'miercoles',hora:'7:00',fecha:'10 marzo 2019',id: uuid()},
+            {titulo: 'Fundamentos JS', dia: 'jueves',hora:'8:00',fecha:'11 marzo 2019',id: uuid()},
+            {titulo:  'React Native', dia: 'viernes',hora:'9:00',fecha:'12 marzo 2019',id: uuid()}
         ],
        tarea: null,
        tareasHechas : []
@@ -34,8 +34,11 @@ class ListaTareas extends Component {
 
     renderItem = ({item}) => {
         return(
-            <View>
-                <Text>{item.dia}</Text>
+            <View style={styles.container}>
+                <Text style={styles.tarea}>{item.titulo}</Text>
+                <Text style={styles.item}>Dia: {item.dia}</Text>
+                <Text style={styles.item}>Hora: {item.hora}</Text>
+                <Text style={styles.item}>Fecha: {item.fecha}</Text>
             </View>
         )
     }
@@ -53,7 +56,21 @@ class ListaTareas extends Component {
     }
 }
 
-
-  
+const styles = StyleSheet.create({
+    container:{
+        padding: 30,
+        margin: 8,
+        backgroundColor: '#f2f2f2',
+    },
+    tarea:{
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 20
+    },
+    item:{
+        fontSize: 16,
+        
+    }
+})
 
 export default ListaTareas
